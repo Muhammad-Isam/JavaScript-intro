1ed181de369af5486f94754022af96c024eb4295
Muhammad-Isam <isam.boy@hotmail.com>
2023-09-01 16:39:22 +0500

    updated

---
+++ app.js
@@ -0,0 +1,60 @@
+// Question 1
+var greet = "Welcome User!";
+alert(greet);
+
+// Question 2
+var eMsg = "Error! Please enter a valid password.";
+alert(eMsg);
+
+// Question 3
+var msg2 = "Welcome to JS Land...\nHappy Coding!";
+alert(msg2);
+
+// Question 4
+var msg3 = "Welcome to JS Land...";
+var msg4 = "Happy Coding!";
+alert(msg3);
+alert(msg4);
+
+// Question 5
+// alert("Hello... I can run JS through my browser's console"); --> In browser
+
+// Question 6
+
+// CHAPTER 2
+
+// Question 1
+var username;
+
+// Question 2
+var myName = "Muhammad Isam";
+
+// Question 3
+var message = "Hello World";
+alert(message);
+
+// Question 4
+var age = "21 years old";
+var skill = "Certified Mobile Application Development";
+alert(myName);
+alert(age);
+alert(skill);
+
+// Question 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Question 6
+var email = "isamboy94@gmail.com";
+alert("My email address is "+email);
+
+// Question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book "+book);
+
+// Question 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Question 9
+var newMessage = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(newMessage);
+document.write(newMessage);
